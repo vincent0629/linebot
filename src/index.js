b7bb391fa89c932854e3ecfd592feb5ce56961fe
Vincent Kao <vincent0629@gmail.com>
2023-03-11 13:31:14 +0800

    Refactor.

--- src/index.js
+++ src/index.js
@@ -1,29 +1,42 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-});
+async function verifyRequest(request) {
+  if (request.method === 'POST') {
+    const input = await request.json();
+    if (input.events && input.events[0].message.type === 'text')
+      return {
+        replyToken: input.events[0].replyToken,
+        text: input.events[0].message.text,
+      };
+  }
+}
 
-function ask(text) {
-  return fetch('https://api.writesonic.com/v2/business/content/chatsonic?engine=premium&language=en', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
-      'X-API-KEY': WRITESONIC_API_KEY,
-    },
-    body: JSON.stringify({
-      'enable_google_results': false,
-      'enable_memory': false,
-      'input_text': text
-    }),
+function chatsonic(text, env) {
+  return new Promise((resolve) => {
+    fetch('https://api.writesonic.com/v2/business/content/chatsonic?engine=premium&language=en', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'X-API-KEY': env.CHATSONIC_API_KEY,
+      },
+      body: JSON.stringify({
+        'enable_google_results': false,
+        'enable_memory': false,
+        'input_text': text
+      }),
+    }).then(resp => resp.json())
+      .then(json => {
+        resolve(json.message);
+      });
   });
 }
 
-function reply(replyToken, text) {
+function reply(replyToken, text, env) {
+  console.log(text);
   return fetch('https://api.line.me/v2/bot/message/reply', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${LINE_ACCESS_TOKEN}`,
+      'Authorization': `Bearer ${env.LINE_ACCESS_TOKEN}`,
     },
     body: JSON.stringify({
       replyToken,
@@ -35,36 +48,29 @@ function reply(replyToken, text) {
   });
 }
 
-function chat(request) {
-  return new Promise((resolve) => {
-    request.json()
-      .then(input => {
-        if (input.events && input.events[0].message.type === 'text') {
-          const replyToken = input.events[0].replyToken;
-          const text = input.events[0].message.text;
-          ask(text)
-            .then(resp => resp.json())
-            .then(json => reply(replyToken, json.message))
-            .then(resp => resp.json())
-            .then(json => {
-              resolve('OK');
-            });
-        } else {
-          resolve('Invalid input');
-        }
-      });
-  });
-}
-
-async function handleRequest(request) {
+async function handleRequest(request, env) {
   let resp;
 
   const { pathname } = new URL(request.url);
-  if (request.method === 'POST' && pathname === '/')
-    resp = chat(request);
-  else
-    resp = Promise.resolve('Invalid request');
+  const input = await verifyRequest(request);
+  if (input) {
+    let promise;
+    if (pathname === '/chatsonic')
+      promise = chatsonic(input.text, env);
+    if (promise) {
+      await promise
+        .then(text => reply(input.replyToken, text, env));
+      resp = new Response('OK');
+    }
+  }
 
-  resp = new Response(await resp);
+  if (!resp)
+    resp = new Response('Invalid request');
   return resp;
 }
+
+export default {
+  fetch(request, env) {
+    return handleRequest(request, env);
+  }
+};
